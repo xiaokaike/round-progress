688debfbcc6b90fc265cda4018489d25e5fb18b0
xiaokai <kexiaokai@gmail.com>
2014-09-11 01:19:42 +0800

    del jq and creat svg by document

--- src/js/svg-round-progress-bar.js
+++ src/js/svg-round-progress-bar.js
@@ -1,13 +1,13 @@
-$(function() {
-    var makeSVGRoundProgressBar = function($this) {
-        var progress = $this.attr("data-progress") ? parseInt($this.attr("data-progress")) : 0,
-            gradientStart = $this.attr("data-progress-gradient-start"),
-            gradientStop = $this.attr("data-progress-gradient-stop"),
-            background = $this.attr("data-progress-background"),
-            strokeWidth = $this.attr("data-progress-stroke-width") ? parseInt($this.attr("data-progress-stroke-width")) : 5,
-            size = $this.attr("data-progress-size") ? parseInt($this.attr("data-progress-size")) : 0;
-
-        var progressID = "progress-bar-" + Math.round(Math.random() * 10000);
+(function() {
+    var makeSVGRoundProgressBar = function(svgContainer) {
+        var progress = 100,
+            // gradientStart = 'rgb(11,178,180)',
+            // gradientStop = 'rgb(0,255,114)',
+            gradientStart = null,
+            gradientStop = null,
+            background = 'rgb(0,255,255)',
+            strokeWidth = 5,
+            size = 200;
 
         // Calculate Size
         progress = progress > 99.9999 ? 99.9999 : progress;
@@ -27,43 +27,38 @@ $(function() {
             "A " + radius + ", " + radius, 0, arc, 1, x, y
         ].join(",");
 
-        // Render Template
-        var svgTemplate = ['<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="', size, 'px" height="', size, 'px">'];
 
-        if (gradientStart && gradientStop) {
-            svgTemplate.push('<defs>',
-                '<linearGradient id="', progressID, '-gradient" x1="50%" y1="0%" x2="50%" y2="100%">',
-                '<stop offset="0%" style="stop-color:', gradientStart, ';stop-opacity:1" />',
-                '<stop offset="100%" style="stop-color:', gradientStop, ';stop-opacity:1" />',
-                '</linearGradient>',
-                '</defs>',
-                '<path id="', progressID, '" d="', path, '" fill="none" stroke="url(#', progressID, '-gradient)" stroke-width="', strokeWidth, '" />');
-        } else if (background) {
-            svgTemplate.push('<path id="', progressID, '" d="', path, '" fill="none" stroke="', background, '" stroke-width="', strokeWidth, '" />');
-        } else {
-            background = ["rgb(", Math.round(Math.random() * 255), ",", Math.round(Math.random() * 255), ",", Math.round(Math.random() * 255), ")"].join("");
-            svgTemplate.push('<path id="', progressID, '" d="', path, '" fill="none" stroke="', background, '" stroke-width="', strokeWidth, '" />');
-        }
+        // createSvG
+        var xmlns = "http://www.w3.org/2000/svg";
 
-        svgTemplate.push('</svg>');
+        var svgElem = document.createElementNS(xmlns, 'svg');
+        svgElem.setAttributeNS (null, "width", size);
+        svgElem.setAttributeNS (null, "height", size);
+        // create path
+        var rpath = document.createElementNS (xmlns, "path");
+        rpath.setAttributeNS (null, 'stroke', background);
+        rpath.setAttributeNS (null, 'stroke-width', strokeWidth);
+        rpath.setAttributeNS (null, 'd', path);
+        rpath.setAttributeNS (null, 'fill', "none");
+        rpath.setAttributeNS (null, 'opacity', 1.0);
 
-        svgTemplate = svgTemplate.join("");
+        svgElem.appendChild(rpath);
 
-        $this.html(svgTemplate);
-        var $circle = $this.find("#" + progressID),
-            pathSize = $circle[0].getTotalLength();
+        svgContainer.appendChild(svgElem);
+        var pathSize = rpath.getTotalLength();
 
-        $circle.css("-webkit-transition", "stroke-dashoffset 1s ease-in-out");
-        $circle.css("stroke-dashoffset", pathSize);
-        $circle.css("stroke-dasharray", pathSize);
+        rpath.style.webkitTransition = "stroke-dashoffset 2s ease-in-out";
+        rpath.style.strokeDashoffset = pathSize;
+        rpath.style.strokeDasharray = pathSize;
 
         // Apply the Stroke Dashoffset asynchronously to trigger CSS Animation.
         setTimeout(function() {
-            $circle.css("stroke-dashoffset", 0);
+            rpath.style.strokeDashoffset = 0;
         }, 0);
     };
 
-    $(".progress").each(function() {
-        makeSVGRoundProgressBar($(this));
-    });
-});
+
+    var svgContainer = document.getElementById ("svgContainer");    
+
+    makeSVGRoundProgressBar(svgContainer);
+})();
