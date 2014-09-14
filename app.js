59a8e6a01e94985284a713848627fd726979a39c
xiaokai <kexiaokai@gmail.com>
2014-09-14 15:57:03 +0800

    扁平华目录

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express();
 var fs = require('fs');
 
 
-app.use(express.static(__dirname + '/src/'));
+app.use(express.static(__dirname + '/'));
 
 app.listen(7777);
 console.log('listen 7777');
